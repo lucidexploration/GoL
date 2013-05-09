7e301f3a8d55e245835dde8a46ebe709fd49ced8
Jon J <girbys@gmail.com>
2013-05-08 20:11:34 -0500

    First push
    uploading src files

---
+++ src/Cell.java
@@ -0,0 +1,302 @@
+
+public class Cell {
+
+    public static boolean[][] cellArray;
+    public static boolean[][] updateArray;
+
+    //create the new cell with all cells set to false/dead
+    public Cell() {
+        int i = 0;
+        int z = 0;
+        cellArray = new boolean[76][76];
+        while (i < cellArray.length) {
+            z = 0;
+            while (z < cellArray.length) {
+                cellArray[i][z] = false;
+                z++;
+            }
+            i++;
+        }
+    }
+
+    //flip the cell state. dead if alive. alive if dead.
+    //this is used for the mouseclick event.
+    public static void Flip(int x, int y) {
+        if (cellArray[x][y] == true) {
+            cellArray[x][y] = false;
+        } else {
+            cellArray[x][y] = true;
+        }
+    }
+
+    //set state of temp array to true
+    public static void Kill(int x, int y) {
+        updateArray[x][y] = false;
+    }
+
+    //set state of temp array to false
+    public static void Live(int x, int y) {
+        updateArray[x][y] = true;
+    }
+
+    //returns state. true for alive, false for dead.
+    public static boolean isAlive(int x, int y) {
+        if (cellArray[x][y] == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //see if cells are overpopulated
+    //see if cells can keep living with 2 or 3 neighbors
+    //see if cells die from < 2 neigbors
+    public static boolean canLive(int x, int y) {
+        if (isAlive(x, y) && Neighbors(x, y) == 2
+                || isAlive(x, y) && Neighbors(x, y) == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    //see if a dead cell can come to life
+    public static boolean canRevive(int x, int y) {
+        if (!isAlive(x, y) && Neighbors(x, y) == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    //returns the number of living neighbors a cell has
+    public static int Neighbors(int x, int y) {
+        int count = 0;
+
+        if (x == 0 && y == 0) {
+            if (isAlive(74, 74)) {
+                count++;
+            }
+            if (isAlive(74, y)) {
+                count++;
+            }
+            if (isAlive(74, y + 1)) {
+                count++;
+            }
+            if (isAlive(x, 74)) {
+                count++;
+            }
+            if (isAlive(x, y + 1)) {
+                count++;
+            }
+            if (isAlive(x + 1, 74)) {
+                count++;
+            }
+            if (isAlive(x + 1, y)) {
+                count++;
+            }
+            if (isAlive(x + 1, y + 1)) {
+                count++;
+            }
+            return count;
+        }
+
+        if (x == 74 && y == 74) {
+            if (isAlive(x - 1, y - 1)) {
+                count++;
+            }
+            if (isAlive(x - 1, y)) {
+                count++;
+            }
+            if (isAlive(x - 1, 0)) {
+                count++;
+            }
+            if (isAlive(x, y - 1)) {
+                count++;
+            }
+            if (isAlive(x, 0)) {
+                count++;
+            }
+            if (isAlive(0, y - 1)) {
+                count++;
+            }
+            if (isAlive(0, y)) {
+                count++;
+            }
+            if (isAlive(0, 0)) {
+                count++;
+            }
+            return count;
+        }
+
+        if (x == 0) {
+            if (isAlive(74, y - 1)) {
+                count++;
+            }
+            if (isAlive(74, y)) {
+                count++;
+            }
+            if (isAlive(74, y + 1)) {
+                count++;
+            }
+            if (isAlive(x, y - 1)) {
+                count++;
+            }
+            if (isAlive(x, y + 1)) {
+                count++;
+            }
+            if (isAlive(x + 1, y - 1)) {
+                count++;
+            }
+            if (isAlive(x + 1, y)) {
+                count++;
+            }
+            if (isAlive(x + 1, y + 1)) {
+                count++;
+            }
+            return count;
+        }
+
+        if (y == 0) {
+            if (isAlive(x - 1, 74)) {
+                count++;
+            }
+            if (isAlive(x - 1, y)) {
+                count++;
+            }
+            if (isAlive(x - 1, y + 1)) {
+                count++;
+            }
+            if (isAlive(x, 74)) {
+                count++;
+            }
+            if (isAlive(x, y + 1)) {
+                count++;
+            }
+            if (isAlive(x + 1, 74)) {
+                count++;
+            }
+            if (isAlive(x + 1, y)) {
+                count++;
+            }
+            if (isAlive(x + 1, y + 1)) {
+                count++;
+            }
+            return count;
+        }
+
+        if (x == 74) {
+            if (isAlive(x - 1, y - 1)) {
+                count++;
+            }
+            if (isAlive(x - 1, y)) {
+                count++;
+            }
+            if (isAlive(x - 1, y + 1)) {
+                count++;
+            }
+            if (isAlive(x, y - 1)) {
+                count++;
+            }
+            if (isAlive(x, y + 1)) {
+                count++;
+            }
+            if (isAlive(0, y - 1)) {
+                count++;
+            }
+            if (isAlive(0, y)) {
+                count++;
+            }
+            if (isAlive(0, y + 1)) {
+                count++;
+            }
+            return count;
+        }
+
+        if (y == 74) {
+            if (isAlive(x - 1, y - 1)) {
+                count++;
+            }
+            if (isAlive(x - 1, y)) {
+                count++;
+            }
+            if (isAlive(x - 1, 0)) {
+                count++;
+            }
+            if (isAlive(x, y - 1)) {
+                count++;
+            }
+            if (isAlive(x, 0)) {
+                count++;
+            }
+            if (isAlive(x + 1, y - 1)) {
+                count++;
+            }
+            if (isAlive(x + 1, y)) {
+                count++;
+            }
+            if (isAlive(x + 1, 0)) {
+                count++;
+            }
+            return count;
+        }
+
+        if (isAlive(x - 1, y - 1)) {
+            count++;
+        }
+        if (isAlive(x - 1, y)) {
+            count++;
+        }
+        if (isAlive(x - 1, y + 1)) {
+            count++;
+        }
+        if (isAlive(x, y - 1)) {
+            count++;
+        }
+        if (isAlive(x, y + 1)) {
+            count++;
+        }
+        if (isAlive(x + 1, y - 1)) {
+            count++;
+        }
+        if (isAlive(x + 1, y)) {
+            count++;
+        }
+        if (isAlive(x + 1, y + 1)) {
+            count++;
+        }
+        return count;
+    }
+
+    //step the array forward one generation and update the changes to the
+    //cellArray
+    public static void Step() {
+        //create the new temp array to store changes.
+        updateArray = new boolean[75][75];
+        //cycle through the cellArray to update the updateArray
+        int i = 0;
+        int z = 0;
+        while (i < updateArray.length) {
+            z = 0;
+            while (z < updateArray.length) {
+                if (canLive(i, z)) {
+                    Live(i, z);
+                }
+                if (canRevive(i, z)) {
+                    Live(i, z);
+                }
+                z++;
+            }
+            i++;
+        }
+        //now copy the updateArray over the cellArray
+        int p = 0;
+        int q = 0;
+        while (p < updateArray.length) {
+            q = 0;
+            while (q < updateArray.length) {
+                cellArray[p][q] = updateArray[p][q];
+                q++;
+            }
+            p++;
+        }
+    }
+}
---
+++ src/CellGUI.java
@@ -0,0 +1,132 @@
+
+//import all the stuff we need/might need
+//i'm not really sure i use all these things, but i know
+//during planning that i thought i might at one time or another
+import javax.swing.*;
+//import for events
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//import for color
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+//mouse things
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+//keyboard things
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+//import a timer
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CellGUI extends JFrame implements ActionListener {
+    //these are the colors ill be used
+    //green for alive
+    //black for dead
+
+    private static Color green = Color.green;
+    private static Color black = Color.black;
+    private static Cell theGame;
+    //the components of the frame
+    private static JFrame frame;
+    private static JLabel label;
+    //a boolean to track if we have started
+    private static boolean isStarted = false;
+    //create the timer for the clock
+    //this counts down time
+    public static Timer timer = new Timer();
+    public static TimerTask task = new TimerTask() {
+        public void run() {
+            if (isStarted) {
+                theGame.Step();
+                label.repaint();
+            }
+        }
+    };
+
+    //add all the buttons and labels to the display
+    public static void addToPane(Container pane) {
+        //add the label im going to draw all over
+        label = new JLabel() {
+            //now paint the darn label with appropriate graphics
+            public void paintComponent(Graphics g) {
+                int i = 0;
+                int z = 0;
+                while (i < theGame.cellArray.length) {
+                    z = 0;
+                    while (z < theGame.cellArray.length) {
+                        g.setColor(Color.black);
+                        if (theGame.isAlive(i, z)) {
+                            g.setColor(green);
+                            g.fillRect(i * 10, z * 10, 10, 10);
+                        }
+                        if (!theGame.isAlive(i, z)) {
+                            g.setColor(black);
+                            g.fillRect(i * 10, z * 10, 10, 10);
+                        }
+                        z++;
+                    }
+                    i++;
+                }
+            }
+        };
+
+        //now we add an actioneventlistener that will call repaint()
+        //it will get event location, figure out which square, and flip()
+        //then call repaint()
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int posX = e.getX();
+                int posY = e.getY();
+                theGame.Flip(posX / 10, posY / 10);
+                label.repaint();
+            }
+        });
+
+
+        //set size of panel to 750x750 aloting 5x5 pixel squares for eacch
+        //array slot
+        label.setPreferredSize(new Dimension(750, 750));
+        frame.getContentPane().add(label);
+    }
+
+    //make the gui and make it look nice
+    public static void makeGUI() {
+        frame = new JFrame("Jon's New Game Of Life - Hit 's' To Start/Stop");
+        theGame = new Cell();
+        timer.schedule(task, 0, 500);
+        frame.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_S) {
+                    if (!isStarted) {
+                        isStarted = true;
+                    } else {
+                        isStarted = false;
+                    }
+                }
+            }
+        });
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addToPane(frame.getContentPane());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    //call everything to create the gui
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                makeGUI();
+            }
+        });
+    }
+
+    //add the event listener to listen
+    //it wouldnt compile without this empty method so i had to
+    //include it.
+    public void actionPerformed(ActionEvent evt) {
+    }
+}
